535d018cc9fa75efcc2a043fc9664f767a0d62d1
Kanishk Gupta <51890299+kanishkgupta2000@users.noreply.github.com>
2022-03-01 18:16:27 +0530

    tryc

--- lib/utils/post_process.py
+++ lib/utils/post_process.py
@@ -107,8 +107,10 @@ def re_ranking(probFea, galFea, k1, k2, lambda_value, local_distmat=None, only_l
     print("done 4")
     print(original_dist.shape)
     # V = np.zeros_like(original_dist).astype(np.float16)
-    V = np.zeros(original_dist.shape ).astype(np.float16)
-
+    try:
+        V = np.zeros((32341,32341)).astype(np.float16)
+    except:
+        print("issue")
     print(V.shape)
     print("done 5")
     initial_rank = np.argsort(original_dist).astype(np.int32)
